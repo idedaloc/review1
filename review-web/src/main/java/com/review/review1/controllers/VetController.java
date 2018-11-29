429d0781a04932a4437e243218da9fda5c63f06f
dedaloc2 <joganel@hotmail.com>
2018-11-29 13:06:54 -0600

    Showing list of vets. Closes #15

--- review-web/src/main/java/com/review/review1/controllers/VetController.java
+++ review-web/src/main/java/com/review/review1/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.review.review1.controllers;
 
+import com.review.review1.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"vets","/vets","vets/index","vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
