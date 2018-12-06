a3b5041ab3176d850e30631f990df3458f418eb8
dedaloc2 <joganel@hotmail.com>
2018-12-06 11:08:59 -0600

    Adding speciality entity

---
+++ review-data/src/main/java/com/review/review1/model/Specialty.java
@@ -0,0 +1,13 @@
+package com.review.review1.model;
+
+public class Specialty extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
