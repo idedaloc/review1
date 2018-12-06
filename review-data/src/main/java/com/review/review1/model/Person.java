be9f5b48e817066093d1ea135f6cc8aa4364cc78
dedaloc2 <joganel@hotmail.com>
2018-12-06 11:11:18 -0600

    Adding personal info propertiess to person entity

--- review-data/src/main/java/com/review/review1/model/Person.java
+++ review-data/src/main/java/com/review/review1/model/Person.java
@@ -1,8 +1,35 @@
 package com.review.review1.model;
 
 public class Person extends BaseEntity {
+    private String address;
+    private String city;
+    private String telephone;
     private String firstName;
     private String lastName;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public String getFirstName() {
         return firstName;
