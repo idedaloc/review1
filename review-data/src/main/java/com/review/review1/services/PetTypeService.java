6f6aa903f2464e6013672cc9ef29c55b882bfac5
dedaloc2 <joganel@hotmail.com>
2018-12-06 11:21:23 -0600

    Creating pet type map service

---
+++ review-data/src/main/java/com/review/review1/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.review.review1.services;
+
+import com.review.review1.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
---
+++ review-data/src/main/java/com/review/review1/services/map/PetTypeMap.java
@@ -0,0 +1,33 @@
+package com.review.review1.services.map;
+
+import com.review.review1.model.PetType;
+import com.review.review1.services.PetTypeService;
+
+import java.util.Set;
+
+class PetPetTypeypeMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
