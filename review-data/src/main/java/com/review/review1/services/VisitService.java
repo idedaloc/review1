cec0d4f2ca1d327d9ae03ec26cae40e09121e6a0
dedaloc2 <joganel@hotmail.com>
2018-12-07 19:02:29 -0600

    Adding visit services

---
+++ review-data/src/main/java/com/review/review1/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.review.review1.services;
+
+import com.review.review1.model.Visit;
+import com.review.review1.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
