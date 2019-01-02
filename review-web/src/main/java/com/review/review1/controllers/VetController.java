116498f1a175a30f899c73de592d019e777273a0
dedaloc2 <joganel@hotmail.com>
2019-01-02 10:12:51 -0600

    Api endpoint example

--- review-web/src/main/java/com/review/review1/controllers/VetController.java
+++ review-web/src/main/java/com/review/review1/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.review.review1.controllers;
 
+import com.review.review1.model.Vet;
 import com.review.review1.services.VetService;
+
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class VetController {
@@ -18,5 +23,12 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+    
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+    	
+    	return vetService.findAll();
+    	
     }
 }
